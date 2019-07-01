31cbf1ac8b61c7600f2c3657d2522403b6252183
ElenaShevtsova <helena_shevts@mail.ru>
2019-07-01 09:56:02 +0300

    Homework9

---
+++ Lesson10/main.js
@@ -0,0 +1,26 @@
+var initialObj = {
+    string: 'Vasya',
+    number: 30,
+    boolean: true,
+    undefined: undefined,
+    null: null,
+    array: [1, 2, 3],
+    object: {
+        string2: 'Petrov',
+        object2: {
+            array2: [{}, {}]
+        },
+        object3: {}
+    },
+    method: function () {
+        alert('Hello');
+    }
+};
+
+var clonedObj = JSON.parse(JSON.stringify(initialObj));
+
+clonedObj.object.object2.array2[1].name = 'Vasya';
+clonedObj.array.push(2);
+
+console.log(initialObj);
+console.log(clonedObj);
